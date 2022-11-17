1a910c1648f6d55305e318d9cba22d6b533f35d2
eun0leee <eun0leee@gmail.com>
2022-11-18 00:41:49 +0900

    feat: add feature
    - more button click
    - select option button click

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 ;(async () => {
-  // 초기화
+  // 초기화 코드
   const inputEl = document.querySelector(".search-input");
+  const selectPageEl = document.querySelector(".page");
   const searchBtnEl = document.querySelector(".search-btn");
   const moviesEl = document.querySelector(".movies");
+  const moreBtnEl = document.querySelector(".btn");
   let title;
+  let page;
   
   // search 버튼 클릭
   searchBtnEl.addEventListener("click", async (e) => {
@@ -14,7 +17,19 @@
     }
 
     title = inputEl.value;
-    const movies = await getMovies(title);
+    const pagenum = selectPageEl.value; // select option 설정
+    for(let i = 1; i <= pagenum; i ++) {
+      page = i;
+      const movie = await getMovies(title, page);
+      renderMovies(movie);
+    }
+  });
+
+  // 더보기 버튼 클릭
+  moreBtnEl.addEventListener("click", async () => {
+    title = inputEl.value;
+    page += 1;
+    const movies = await getMovies(title, page);
     renderMovies(movies);
   });
 
@@ -33,9 +48,6 @@
       
       const h3El = document.createElement("h3");
       h3El.textContent = movie.Title;
-      h3El.addEventListener("click", () => {
-        console.log(movie.Title);
-      });
 
       const imgEl = document.createElement("img");
       imgEl.src = movie.Poster;
