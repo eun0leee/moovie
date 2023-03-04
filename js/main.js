8bc63f38152fa098f5bc9a56516c4ac0fc3bee90
eun0leee <eun0leee@gmail.com>
2023-03-04 22:40:16 +0900

    Feat : 포스터 hover시에 info 보여주기

--- js/main.js
+++ js/main.js
@@ -25,12 +25,18 @@ moviesEl.className = 'movies';
 
 function renderMovies(movies) {
   beforeType.classList.add('hide');
+  resultsEl.classList.add('afterType');
+
   if (movies.Response === 'True') {
     movies.Search.map((movie) => {
       const movieLiEl = document.createElement('li');
       movieLiEl.className = 'movie';
       movieLiEl.innerHTML = `
         <img src='${movie.Poster}' alt='${movie.Title}의 포스터'/>
+        <div class='info'>
+        <p>${movie.Title}</p>
+        <p>${movie.Year}</p>
+        </div>
         `;
 
       moviesEl.append(movieLiEl);
