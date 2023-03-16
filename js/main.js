547c0e100e60f9071e080f598d9bbed6dfcfcdf9
eun0leee <eun0leee@gmail.com>
2023-03-16 21:45:14 +0900

    Feat : 모듈화

--- js/main.js
+++ js/main.js
@@ -1,56 +1,16 @@
 import { getMovies } from './api';
-
-const searchFormEl = document.querySelector('form');
-const searchInputEl = document.querySelector('.search-input');
-const selectCountEl = document.querySelector('.select-count');
-const selectYearEl = document.querySelector('.select-year');
-const beforeType = document.querySelector('.beforeType');
+import {
+  searchFormEl,
+  searchInputEl,
+  selectCountEl,
+  selectYearEl,
+  moviesEl,
+  makeOptionValue,
+} from './store';
+import renderMovies from './render';
 
 // 개봉년도 옵션 생성
-const makeOptionValue = () => {
-  let currentYear = new Date().getFullYear();
-  for (let i = currentYear; i >= 1980; i--) {
-    const optionYearEl = document.createElement('option');
-    optionYearEl.value = i;
-    optionYearEl.innerText = i;
-    selectYearEl.append(optionYearEl);
-  }
-};
 makeOptionValue();
-
-// 렌더링
-const resultsEl = document.querySelector('.results');
-const moviesEl = document.createElement('ul');
-moviesEl.className = 'movies';
-
-function renderMovies(movies) {
-  beforeType.classList.add('hide');
-  resultsEl.classList.add('afterType');
-
-  if (movies.Response === 'True') {
-    movies.Search.map((movie) => {
-      const movieLiEl = document.createElement('li');
-      movieLiEl.className = 'movie';
-      movieLiEl.innerHTML = `
-        <img src='${movie.Poster}' alt='${movie.Title}의 포스터'/>
-        <div class='info'>
-        <p>${movie.Title}</p>
-        <p>${movie.Year}</p>
-        </div>
-        `;
-
-      moviesEl.append(movieLiEl);
-      resultsEl.append(moviesEl);
-    });
-  } else if (movies.Response === 'False') {
-    resultsEl.innerHTML = '';
-    const noResults = document.createElement('p');
-    noResults.className = 'noResults';
-    noResults.innerText =
-      'No search results found.TT \n Try searching with another keyword.';
-    resultsEl.append(noResults);
-  }
-}
 
 // 검색
 const handleSubmit = async (e) => {
@@ -71,20 +31,3 @@ const handleSubmit = async (e) => {
 searchFormEl.addEventListener('submit', handleSubmit);
 
 // 로딩
-// const loading = document.querySelector('.loading');
-// hideLoading();
-
-// function showLoading() {
-//   loading.style.display = 'block';
-// }
-// function hideLoading() {
-//   loading.style.display = 'none';
-// }
-
-// const infoEl = document.createElement('div');
-// infoEl.classList.add('infoEl');
-// const titleEl = document.createElement('h3');
-// titleEl.textContent = movie.Title;
-// const yearEl = document.createElement('span');
-// yearEl.textContent = movie.Year;
-// infoEl.append(titleEl, yearEl);
---
+++ js/render.js
@@ -0,0 +1,32 @@
+import { moviesEl, beforeType, resultsEl } from './store';
+
+moviesEl.className = 'movies';
+
+export default function renderMovies(movies) {
+  beforeType.classList.add('hide');
+  resultsEl.classList.add('afterType');
+
+  if (movies.Response === 'True') {
+    movies.Search.map((movie) => {
+      const movieLiEl = document.createElement('li');
+      movieLiEl.className = 'movie';
+      movieLiEl.innerHTML = `
+          <img src='${movie.Poster}' alt='${movie.Title}의 포스터'/>
+          <div class='info'>
+          <p>${movie.Title}</p>
+          <p>${movie.Year}</p>
+          </div>
+          `;
+
+      moviesEl.append(movieLiEl);
+      resultsEl.append(moviesEl);
+    });
+  } else if (movies.Response === 'False') {
+    resultsEl.innerHTML = '';
+    const noResults = document.createElement('p');
+    noResults.className = 'noResults';
+    noResults.innerText =
+      'No search results found.TT \n Try searching with another keyword.';
+    resultsEl.append(noResults);
+  }
+}
