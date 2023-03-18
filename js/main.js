089aa6a342cdab008cf2489cefd7e40543493406
eun0leee <eun0leee@gmail.com>
2023-03-18 16:11:16 +0900

    Feat : 무한스크롤 추가

--- js/main.js
+++ js/main.js
@@ -11,11 +11,24 @@ import {
 } from './store';
 import renderMovies from './render';
 
+let page;
+
 // 개봉년도 옵션 생성
 makeOptionValue();
 
+// api get 호출 및 렌더
+const getDataAndRender = async () => {
+  const response = await getMovies(
+    searchInputEl.value,
+    page,
+    selectYearEl.value
+  );
+  loadingEl.classList.remove('show');
+  renderMovies(response);
+};
+
 // 검색
-const handleSubmit = async (e) => {
+const handleSubmit = (e) => {
   e.preventDefault();
 
   beforeType.classList.add('hide');
@@ -23,15 +36,19 @@ const handleSubmit = async (e) => {
   moviesEl.innerHTML !== '' ? (moviesEl.innerHTML = '') : '';
 
   for (let i = 1; i <= selectCountEl.value; i++) {
-    let page = i;
-    const response = await getMovies(
-      searchInputEl.value,
-      page,
-      selectYearEl.value
-    );
-    loadingEl.classList.remove('show');
-    renderMovies(response);
+    page = i;
+    getDataAndRender();
   }
 };
 
 searchFormEl.addEventListener('submit', handleSubmit);
+
+// 무한스크롤
+const infinite = async () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    page += 1;
+    getDataAndRender();
+  }
+};
+
+window.addEventListener('scroll', infinite);
