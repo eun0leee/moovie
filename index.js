061523f250f26ffd2bd5de602136ba7362b802e6
eun0leee <eun0leee@gmail.com>
2023-05-18 15:37:57 +0900

    feat : pjax SPA 라우팅_spa 렌더링

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 import search from '/src/pages/Search/index';
 import detail from '/src/pages/Detail/index';
 
-const router = async () => {
-  // 라우터 배열
-  const routes = [
-    { path: '', view: search },
-    {
-      path: 'detail',
-      view: async () => {
-        await detail(location.pathname.split('/')[2]);
-      },
-    },
-  ];
+search();
 
-  // 라우터 매치
-  const routerMatchs = routes.map((route) => {
-    return {
-      route,
-      isMatch: location.pathname.split('/')[1] === route.path,
-    };
-  });
+document.addEventListener('urlchange', async (e) => {
+  let url = e.target.baseURI;
+  let path = new URL(url).pathname;
+  const searchParams = new URL(url).searchParams;
+  const queryString = searchParams.get('id');
 
-  let match = routerMatchs.find((routerMatch) => routerMatch.isMatch);
-
-  if (!match) {
-    match = { route: routes[0], isMatch: true };
+  switch (path) {
+    case '/':
+      search();
+      break;
+    case '/detail/':
+      await detail(queryString);
+      break;
+    default:
   }
-
-  // 매치된 내용 출력
-  match.route.view();
-};
-
-const navigateTo = (url) => {
-  history.pushState(null, null, url);
-  router();
-};
-
-navigateTo(location.href);
-
-window.addEventListener('popstate', router);
+});
